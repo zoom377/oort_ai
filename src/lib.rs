8a3cf23828e72f805cc571d7b40d3133d57f2592
Tommy Evans <zoom377@googlemail.com>
2023-11-14 12:35:00 +0000

    init

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+// Tutorial: Radar
+// Destroy the enemy ships. Use your radar to find them.
+// Hint: Press 'g' in-game to show where your radar is looking.
+// Hint: Press 'n' to single-step.
+// Hint: Use the set_radar_heading() function to keep your radar pointed at a
+// target, or to search for a new one.
+//
+// Join the Discord at https://discord.gg/vYyu9EhkKH for Oort discussion and
+// tournament results.
+use oort_api::prelude::*;
+
+const BULLET_SPEED: f64 = 1000.0; // m/s
+
+pub struct Ship {}
+
+impl Ship {
+    pub fn new() -> Ship {
+        Ship {}
+    }
+
+    pub fn tick(&mut self) {
+        set_radar_heading(radar_heading() + radar_width());
+        if let Some(contact) = scan() {
+            accelerate(0.1 * (contact.position - position()));
+            fire(0);
+        }
+    }
+}
